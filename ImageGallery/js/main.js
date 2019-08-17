c7bd50a9afde50ce9e5da31a4d10caa2eaac0bbd
NEET-Book <yanoerika@163.com>
2019-08-17 21:50:43 +0800

    修改页面结构，修改页面样式

--- ImageGallery/js/main.js
+++ ImageGallery/js/main.js
@@ -4,7 +4,7 @@ function showPic(whichpic){
     var whichpic = event.target;    //获得被点击的a元素
     var source = whichpic.getAttribute("href");    //创建变量保存显示图片的href
     var placeholder = document.getElementById("placeholder");   //获得占位图片
-    placeholder.setAttribute("scr",source);
+    placeholder.setAttribute("src",source);
 }
 
 function showPicDOM1( ){ //使用非DOM方法
@@ -13,9 +13,6 @@ function showPicDOM1( ){ //使用非DOM方法
     var placeholder = document.getElementById("placholder");
     placeholder.src = source;       //不使用DOM方法来给占位图src属性赋值
 }
-
-var text = document.getElementsByTagName("a");
-console.log(text[0].text);
 var ul = document.getElementsByTagName("ul")[0];
 ul.onclick = function(){
     var whichpic = event.target
